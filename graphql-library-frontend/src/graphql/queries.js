68a2c8ee9fc6426a334b40b55537a3b77013d68f
Kristoffer Masajo <toffet-masajo@users.noreply.github.com>
2024-02-22 02:59:15 +0800

    8.17 Listing books

--- graphql-library-frontend/src/graphql/queries.js
+++ graphql-library-frontend/src/graphql/queries.js
@@ -14,7 +14,9 @@ export const ALL_BOOKS = gql`
   query {
     allBooks {
       title
-      author
+      author {
+        name
+      }
       published
     }
   }
@@ -34,7 +36,9 @@ export const ADD_BOOK = gql`
       genres: $genres
     ) {
       title
-      author
+      author {
+        name
+      }
       published
       genres
     }
