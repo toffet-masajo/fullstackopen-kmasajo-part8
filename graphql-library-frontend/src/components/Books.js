9102b98c894fe4282c77f5d6e47518fcb6aef6b6
Kristoffer Masajo <toffet-masajo@users.noreply.github.com>
2024-02-24 02:17:04 +0800

    8.19 Books by genre, part 1

--- graphql-library-frontend/src/components/Books.js
+++ graphql-library-frontend/src/components/Books.js
@@ -1,8 +1,10 @@
 import { useQuery } from "@apollo/client";
+import { useState } from "react";
 
 import { ALL_BOOKS } from "../graphql/queries";
 
 const Books = (props) => {
+  const [filter, setFilter] = useState("all genres");
   const result = useQuery(ALL_BOOKS);
 
   if (!props.show) {
@@ -14,6 +16,15 @@ const Books = (props) => {
   }
 
   const books = result.data.allBooks;
+  const genres = books.reduce(
+    (accumulator, { genres }) => {
+      genres.forEach((genre) => {
+        if (!accumulator.includes(genre)) accumulator.push(genre);
+      });
+      return accumulator;
+    },
+    ["all genres"]
+  );
 
   return (
     <div>
@@ -26,14 +37,25 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books.map((book) => {
+            if (filter === "all genres" || book.genres.includes(filter))
+              return (
+                <tr key={book.title}>
+                  <td>{book.title}</td>
+                  <td>{book.author.name}</td>
+                  <td>{book.published}</td>
+                </tr>
+              );
+            return null;
+          })}
         </tbody>
+        <div>
+          {genres.map((genre, idx) => (
+            <button key={idx} onClick={() => setFilter(genre)}>
+              {genre}
+            </button>
+          ))}
+        </div>
       </table>
     </div>
   );
